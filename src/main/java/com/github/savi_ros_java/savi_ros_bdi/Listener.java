985b1e3b804c80a8a8780210a8ebdd4218d7b4ff
Unknown <pgavigan@gmail.com>
2019-12-06 13:36:55 -0500

    Fixed minor compile error (missing declaration)

--- src/main/java/com/github/savi_ros_java/savi_ros_bdi/Listener.java
+++ src/main/java/com/github/savi_ros_java/savi_ros_bdi/Listener.java
@@ -36,7 +36,7 @@ import jason.asSyntax.*;
 public class Listener extends AbstractNodeMain {
 
     private SyncAgentState agentState;
-    //private SaviAgentArch agent;
+    private SaviAgentArch agent;
 
     /**
      * Provide name of this node when requested.
