9676bf072659c29574981c10b3e01fc28759f6c4
Unknown <pgavigan@gmail.com>
2019-12-06 15:06:07 -0500

    Added some debug outputs

--- src/main/java/com/github/savi_ros_java/savi_ros_bdi/SyncAgentState.java
+++ src/main/java/com/github/savi_ros_java/savi_ros_bdi/SyncAgentState.java
@@ -78,6 +78,7 @@ public class SyncAgentState {
     */
     public synchronized void setPerceptions(String newPerception) {
         this.perception = String.valueOf(newPerception);
+        System.out.println("Perception logged at SyncAgentState using setPerceptions()");
     }
 
 
@@ -99,8 +100,10 @@ public class SyncAgentState {
      */
     public synchronized String getPerceptions() {
         if (this.perception == null) {
+            System.out.println("NULL Perception read at SyncAgentState using getPerceptions()");
             return null;
         } else {
+            System.out.println("A REAL Perception read at SyncAgentState using getPerceptions()");
             return this.perception;
         }
     }
