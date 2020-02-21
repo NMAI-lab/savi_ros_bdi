673f9c703709d5d91e1c97b392e874aee18bb6ca
Patrick Gavigan <pgavigan@gmail.com>
2020-02-21 12:02:17 -0500

    Working on adding a tester

---
+++ src/test/java/savi_ros_java/savi_ros/bdi/agent_state/SyncAgentStateTester.java
@@ -0,0 +1,11 @@
+package savi_ros_java.savi_ros.bdi.agent_state;
+
+import org.junit.jupiter.api.Test;
+
+public class SyncAgentStateTester {
+
+    @Test
+    void test() {
+        System.out.println("Hello from the tester");
+    }
+}
