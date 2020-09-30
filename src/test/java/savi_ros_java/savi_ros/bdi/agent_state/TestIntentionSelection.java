81413b123657486e728eaa627c9aad1430689749
Patrick Gavigan <pgavigan@gmail.com>
2020-09-30 10:00:17 -0400

    Setting up test of intention selection function

---
+++ src/test/java/savi_ros_java/savi_ros/bdi/agent_state/TestIntentionSelection.java
@@ -0,0 +1,12 @@
+package savi_ros_java.savi_ros.bdi.agent_state;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class TestIntentionSelection extends TestCase {
+
+    @Test
+    public void testCase() {
+        System.out.println("Test case!");
+    }
+}
