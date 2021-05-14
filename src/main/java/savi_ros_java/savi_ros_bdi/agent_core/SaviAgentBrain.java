bfa8b33c461e21d7d94b6b6f30f8eb3466119fed
Patrick Gavigan <pgavigan@gmail.com>
2021-05-14 09:48:02 -0400

    Updating agent brain to latest framework

--- src/main/java/savi_ros_java/savi_ros_bdi/agent_core/SaviAgentBrain.java
+++ src/main/java/savi_ros_java/savi_ros_bdi/agent_core/SaviAgentBrain.java
@@ -1,22 +1,29 @@
 package savi_ros_java.savi_ros_bdi.agent_core;
 
-import jason.asSemantics.*;
-import jason.asSyntax.*;
-import java.util.*;
-import java.util.regex.*;
+import jason.asSemantics.Agent;
+import jason.asSemantics.Event;
+import jason.asSemantics.Option;
+import jason.asSyntax.Literal;
+import jason.asSyntax.LogicalFormula;
+import jason.asSyntax.Plan;
+import jason.asSyntax.Term;
+import jason.bb.BeliefBase;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
 
 public class SaviAgentBrain extends Agent {
 
+    protected List<String> eventPriorities = Arrays.asList("safety", "health", "map", "mission", "navigation", "movement");
+
     /**
      * Highest priority event has priority
-     * Ex: [priotiy(1)]
-     * Highest priority is smallest integer greater than 0
      * Otherwise, first event in the queue is chosen.
      */
     @Override
     public Event selectEvent(Queue<Event> events) {
-        //System.out.println("I'm in the event selection function!");
-
         Event selected;
         if (events.size() > 1) {
             selected = this.getHighestPriorityEvent(events);
@@ -35,8 +42,6 @@ public class SaviAgentBrain extends Agent {
      */
     @Override
     public Option selectOption(List<Option> options) {
-        //System.out.println("I'm in the option selection function!");
-        //System.out.println(options.toString());
         Option selected;
 
         if (options.size() > 1) {
@@ -52,69 +57,33 @@ public class SaviAgentBrain extends Agent {
     }
 
 
-
-    /**
-     * Highest priority intention has priority
-     * Ex: [priotiy(1)]
-     * Highest priority is smallest integer greater than 0
-     * Otherwise, first intention in the queue is chosen.
-     */
-    @Override
-    public Intention selectIntention(Queue<Intention> intentions) {
-        //System.out.println("I'm in the intention selection function!");
-        //System.out.println(intentions.toString());
-        Intention selected;
-
-        if (intentions.size() > 1) {
-            selected = this.getHighestPriorityIntention(intentions);
-        } else {
-            selected = super.selectIntention(intentions);
-        }
-
-        //System.out.println(selected.toString());
-        intentions.remove(selected);
-        return selected;
-    }
-
     /**
      * Return the highest priority event from the events list.
      */
     protected Event getHighestPriorityEvent(Queue<Event> events) {
-        int priority = -1;    // The "champion" -> the best priority we've found so far (-1 is init)
-        Event priorityEvent = null;    // The event that has the highest priority so far
 
-        // Iterate through the list, look for the "champion"
-        Iterator<Event> eventInstance = events.iterator();
-        while (eventInstance.hasNext()) {
-            Event current = eventInstance.next();
-            List<Term> annotList = current.getTrigger().getLiteral().getAnnots();
-            int currentPriority = this.getPriority(annotList);
-
-            // Deal with first iterantion, need to not have null
-            if (priorityEvent == null) {
-                priorityEvent = current;
-                priority = currentPriority;
-            }
-
-            // Looking for priority. Smallest value greater than 0 is highest
-            // priority
-            if ((currentPriority > 0) && (currentPriority < priority)) {
-                priorityEvent = current;
-                priority = currentPriority;
-            }
-
-            // Special case, if we found a priority 1 case (highest priority)
-            // then we don't need to look any further
-            if (priority == 1) {
-                break;
+        BeliefBase beliefs = this.getBB();
+        for (String priority : eventPriorities) {
+            for (Event event : events) {
+                String trigger = event.getTrigger().getLiteral().getFunctor();
+                for (Literal belief : beliefs) {
+                    if (belief.getFunctor().contentEquals(priority)) {
+                        List<Term> terms = belief.getTerms();
+                        for (Term term : terms) {
+                            if (term.toString().contentEquals(trigger)) {
+                                //System.out.println("Chosen event: " + event.toString());
+                                return event;
+                            }
+                        }
+                    }
+                }
             }
         }
-        //System.out.println("Selected event: " + priorityEvent.toString() + " with priority: " + Integer.toString(priority));
-        //events.remove(priorityEvent);
-        return priorityEvent;
+
+        // If I made it here, use the default
+        //System.out.println("Using the default event");
+        return super.selectEvent(events);
     }
-
-
 
     /**
      * Return the highest priority option from the options list.
@@ -123,7 +92,6 @@ public class SaviAgentBrain extends Agent {
      * 		Need a way to choose between non default options
      */
     protected Option getHighestPriorityOption(List<Option> options) {
-
 
         Option priorityOption = null;    // The event that has the highest priority so far
 
@@ -154,68 +122,5 @@ public class SaviAgentBrain extends Agent {
         }
 
         return priorityOption;
-    }
-
-
-    /**
-     * Return the highest priority intention from the intentions list.
-     */
-    protected Intention getHighestPriorityIntention(Queue<Intention> intentions) {
-        int priority = -1;    // The "champion" -> the best priority we've found so far (-1 is init)
-        Intention priorityIntention = null;    // The event that has the highest priority so far
-
-        // Iterate through the list, look for the "champion"
-        Iterator<Intention> intentionInstance = intentions.iterator();
-        while (intentionInstance.hasNext()) {
-            Intention current = intentionInstance.next();
-            Literal myLiteral = Literal.parseLiteral(current.toString());    // This is the one place where the syntax differs from the Event version, otherwise there would not be two versions of this.
-            List<Term> annotList = myLiteral.getAnnots();
-            int currentPriority = this.getPriority(annotList);
-
-            // Deal with first iterantion, need to not have null
-            if (priorityIntention == null) {
-                priorityIntention = current;
-                priority = currentPriority;
-            }
-
-            // Looking for priority. Smallest value greater than 0 is highest
-            // priority
-            if ((currentPriority > 0) && (currentPriority < priority)) {
-                priorityIntention = current;
-                priority = currentPriority;
-            }
-
-            // Special case, if we found a priority 1 case (highest priority)
-            // then we don't need to look any further
-            if (priority == 1) {
-                break;
-            }
-        }
-        //System.out.println("Selected intention: " + priorityIntention.toString() + " with priority: " + Integer.toString(priority));
-        //intentions.remove(priorityIntention);
-        return priorityIntention;
-    }
-
-    /**
-     * Find the priority annotation and return the value.
-     * Returns -1 if none is found.
-     */
-    protected int getPriority(List<Term> annotList) {
-        int priority = -1;
-        //List<Term> annotList = selected.getTrigger().getLiteral().getAnnots();
-        Iterator<Term> t = annotList.iterator();
-        while (t.hasNext()) {
-            Term current = t.next();
-            String currentString = current.toString();
-            if (currentString.contains("priority")) {
-                Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(currentString);
-                while (m.find()) {
-                    priority = Integer.parseInt(m.group(1));
-                    return priority;
-                }
-            }
-            //System.out.println(annotList.contains(Literal.parseLiteral("priority(1)")));
-        }
-        return priority;
     }
 }
